fa108b27be57fcbe4ddd975067024d1e84a20247
Tomas Lenoch(lenoctom) <info@theatro.sk>
2017-08-14 21:28:43 +0200

    fixed minutes error in text date

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,7 +3,7 @@ import moment from 'moment';
 export function convertDate(timestamp){
     return {
         date: moment.unix(timestamp).format("DD.MM.YYYY"),
-        time: moment.unix(timestamp).format("HH:MM"),
+        time: moment.unix(timestamp).format("HH:mm"),
         full: moment.unix(timestamp)
     }
 }
