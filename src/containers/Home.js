c9f422a377496ec38590629515ad9859f477be7c
Tomas Lenoch(lenoctom) <info@theatro.sk>
2017-08-14 21:27:16 +0200

    added basic styling to the list

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,43 +1,56 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import {loadMatches,selectMatchesFromSport,startLoading} from '../actions';
-import {ListView,Screen,View,Text} from '@shoutem/ui';
+import { connect } from 'react-redux';
+import { loadMatches, selectMatchesFromSport, startLoading } from '../actions';
+import { Image } from 'react-native'
+import { ListView, Screen, View, Text, Card, Subtitle, Caption, Button, Icon } from '@shoutem/ui';
 import SportSelector from '../components/SportSelector';
 
 
 class HomeContainer extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            actualSport:null,
-            loading:true
+        this.state = {
+            actualSport: null,
+            loading: true
         }
     }
-    renderRow(match){
-        console.log(JSON.stringify(match.date));
-        return(
-            <View>
-                <Text>{match.title}</Text>
+    renderRow(match) {
+        //console.log(JSON.stringify(match.date));
+        return (
+            <View style={{ backgroundColor: 'white' , flex:1}}
+                styleName="horizontal v-center">
+                <View style={{flex:60}} styleName="vertical md-gutter">
+                    <Subtitle styleName="multiline v-center">
+                    {match.title}
+                    </Subtitle>
+                    <View styleName="horizontal v-center space-between">
+                        <Caption>{match.competition}</Caption>
+                        <Caption>{match.station}</Caption>
+                    </View>
+                </View>
+                <View style={{flex:40}} styleName="vertical v-start h-center">
+                    <Subtitle>{match.date.time}</Subtitle>
+                </View>
             </View>
         )
     }
     componentDidMount() {
         this.props.loadSport('Futbal');
-       
+
     }
-    handleSportSelection(sport){
-        this.setState({actualSport:sport});
+    handleSportSelection(sport) {
+        this.setState({ actualSport: sport });
         console.log(JSON.stringify(this.state));
         console.log(sport);
         this.props.clearDataAndStartLoading();
         this.props.loadSport(sport.value);
     }
-    
+
     render() {
         console.log(JSON.stringify(this.props));
         return (
-            <Screen>
-                <SportSelector 
+            <Screen style={{ flex: 1 }}>
+                <SportSelector
                     actual={this.state.actualSport}
                     onSelect={this.handleSportSelection.bind(this)}
                 />
@@ -51,17 +64,17 @@ class HomeContainer extends React.Component {
     }
 }
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
     return {
-        loading:state.matches.loading,
-        data:state.matches.data
+        loading: state.matches.loading,
+        data: state.matches.data
     }
 }
-const mapDispatchToProps=(dispatch)=>{
+const mapDispatchToProps = (dispatch) => {
     return {
         load: () => (dispatch(loadMatches())),
         loadSport: (sport) => (dispatch(selectMatchesFromSport(sport))),
         clearDataAndStartLoading: () => (dispatch(startLoading()))
     }
 }
-export default connect(mapStateToProps,mapDispatchToProps)(HomeContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(HomeContainer);
