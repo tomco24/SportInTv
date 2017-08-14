516889c5f272a7d062b6b8b4456524bd5fe88a74
Tomas Lenoch(lenoctom) <info@theatro.sk>
2017-08-14 15:04:47 +0200

    Initial project setup

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {AppLoading,Font}  from  'expo';
+import { StyleSheet, View,StatusBar } from 'react-native';
+import { NavigationBar, Title,Examples} from '@shoutem/ui'
+
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      fontsAreLoaded:false
+    }
+  }
+
+  async componentWillMount() {
+    await Font.loadAsync({
+      'Rubik-Black': require('../node_modules/@shoutem/ui/fonts/Rubik-Black.ttf'),
+      'Rubik-BlackItalic': require('../node_modules/@shoutem/ui/fonts/Rubik-BlackItalic.ttf'),
+      'Rubik-Bold': require('../node_modules/@shoutem/ui/fonts/Rubik-Bold.ttf'),
+      'Rubik-BoldItalic': require('../node_modules/@shoutem/ui/fonts/Rubik-BoldItalic.ttf'),
+      'Rubik-Italic': require('../node_modules/@shoutem/ui/fonts/Rubik-Italic.ttf'),
+      'Rubik-Light': require('../node_modules/@shoutem/ui/fonts/Rubik-Light.ttf'),
+      'Rubik-LightItalic': require('../node_modules/@shoutem/ui/fonts/Rubik-LightItalic.ttf'),
+      'Rubik-Medium': require('../node_modules/@shoutem/ui/fonts/Rubik-Medium.ttf'),
+      'Rubik-MediumItalic': require('../node_modules/@shoutem/ui/fonts/Rubik-MediumItalic.ttf'),
+      'Rubik-Regular': require('../node_modules/@shoutem/ui/fonts/Rubik-Regular.ttf'),
+      'rubicon-icon-font': require('../node_modules/@shoutem/ui/fonts/rubicon-icon-font.ttf'),
+    });
+
+    this.setState({ fontsAreLoaded: true });
+  }
+
+  render() {
+    if (!this.state.fontsAreLoaded) {
+      return <AppLoading />;
+    }
+
+    return (
+        <View style={{flex: 1}}>
+        <Examples />
+        <StatusBar barStyle="default" hidden={false} />
+      </View>
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
